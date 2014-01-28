f1d3bc2089ea9c731e650e50b4d9716bb2878c16
vince-doggyloot <vince@doggyloot.com>
2014-01-28 17:48:01 -0600

    updated tasks

--- inventory_manager/tasks.py
+++ inventory_manager/tasks.py
@@ -11,16 +11,6 @@ class SyncDbWithTrackerDoc(Task):
         rip_doc(doc, API_LIST_URI, API_DETAIL_URI, API_KEY)
 
 
-class TestSpam(Task):
-    def run(self, *args, **kwargs):
-        import smtplib
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()
-        server.login('v.forgione@gmail.com', '7h3M0n@R(h')
-        server.sendmail('v.forgione@gmail.com', 'vince@doggyloot.com', 'this has been a test')
-        server.quit()
-
-
 if __name__ == '__main__':
     sync = SyncDbWithTrackerDoc()
     sync.run()
