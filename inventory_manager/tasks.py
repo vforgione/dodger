b0b931cdd458807cc1a5aadba2fa178af49138a2
vince-doggyloot <vince@doggyloot.com>
2014-01-29 13:24:06 -0600

    updated gdocs code to use ORM

--- inventory_manager/tasks.py
+++ inventory_manager/tasks.py
@@ -11,7 +11,7 @@ class SyncDbWithTrackerDoc(PeriodicTask):
 
     def run(self, *args, **kwargs):
         doc = read_doc(GDOCS_USER, GDOCS_PASSWD, GDOCS_DOCNAME, GDOCS_SHEET)
-        rip_doc(doc, API_LIST_URI, API_DETAIL_URI, API_KEY)
+        rip_doc(doc)
 
 
 if __name__ == '__main__':
