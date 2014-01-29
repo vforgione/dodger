7435c2add21a9eac752e55862582db26e7ded917
vince-doggyloot <vince@doggyloot.com>
2014-01-29 16:01:06 -0600

    updated IM:tasks

--- inventory_manager/tasks.py
+++ inventory_manager/tasks.py
@@ -1,13 +1,9 @@
-from datetime import timedelta
-
-from celery.task import PeriodicTask
+from celery.task import Task
 from libs.gdocs import read_doc, rip_doc
 from libs.gdoc_config import *
 
 
-class SyncDbWithTrackerDoc(PeriodicTask):
-
-    run_every = timedelta(hours=1)
+class SyncDbWithTrackerDoc(Task):
 
     def run(self, *args, **kwargs):
         doc = read_doc(GDOCS_USER, GDOCS_PASSWD, GDOCS_DOCNAME, GDOCS_SHEET)
