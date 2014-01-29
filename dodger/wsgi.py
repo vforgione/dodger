504e5634a229627e2b9fe8673254fa2a75996ed0
vince-doggyloot <vince@doggyloot.com>
2014-01-29 10:56:34 -0600

    added newrelic monitoring

--- dodger/wsgi.py
+++ dodger/wsgi.py
@@ -10,5 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dodger.settings")
 
+import newrelic.agent
+newrelic.agent.initialize('/home/deploy/dodger-env/newrelic.ini')
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
