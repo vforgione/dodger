9723f4cbcf23181f2a6510aa839df2390fa2a8c9
vince-doggyloot <vince@doggyloot.com>
2014-01-29 13:43:26 -0600

    updated gdocs code to use ORM

--- inventory_manager/libs/gdocs.py
+++ inventory_manager/libs/gdocs.py
@@ -20,6 +20,7 @@ import re
 
 from django.template.defaultfilters import slugify
 import gspread
+import pytz
 
 
 logger = logging.getLogger('gdocs')
@@ -126,6 +127,7 @@ def rip_doc(doc):
                 datetime.strptime(r['Date Received / Updated'], '%m/%d/%Y') for r in rows
                 if re.match('\d{2}/\d{2}/\d{4}', r['Date Received / Updated'])
             ])
+            doc_mod.replace(tzinfo=pytz.timezone('US/Central'))
             return doc_mod
         except ValueError:
             return None
