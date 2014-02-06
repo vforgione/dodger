0a8ff3491c0e18c961167e0fa4e04bdde37fa4ea
vforgione <v.forgione@gmail.com>
2014-02-06 10:29:12 -0600

    updated receive order form

--- apps/warehouse/static/warehouse/js/shipment-create.js
+++ apps/warehouse/static/warehouse/js/shipment-create.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
         dataType: 'json',
         async: false,  // make blocking call
         success: function(xhr, status){
+          // set supplier to retrieve products
           var supplier_uri = xhr.supplier;
           $.ajax({
             url: supplier_uri,
@@ -40,6 +41,32 @@ $(document).ready(function(){
         error: function(xhr, status){ alert(status + ' ' + xhr.responseText); },
         beforeSend: function(xhr){ xhr.setRequestHeader('Authorization', 'ApiKey vince:51ec739d7e8e7a24381ebd8b8a10e6be79c33e54'); },
         complete: function(){ $('select.product').html(options); }
+      });
+
+      // get order products info for info pane
+      $.ajax({
+        url: '/api/dat/purchase-order-products/?purchase_order__id=' + $(this).val(),
+        type: 'GET',
+        dataType: 'json',
+        success: function(xhr, status){
+          var products = '<h3>Order Details</h3><table class="table table-striped"><thead><tr><th>Product</th><th>Qty Ordered</th>';
+          $(xhr.objects).each(function(i){
+            $.ajax({
+              url: xhr.objects[i].product,
+              type: 'GET',
+              dataType: 'json',
+              async: false,
+              success: function(xhr, status){ products += '<tr><td>' + xhr.description + '</td>'; },
+              error: function(xhr, status){ alert(status + ' ' + xhr.responseText); },
+              beforeSend: function(xhr){ xhr.setRequestHeader('Authorization', 'ApiKey vince:51ec739d7e8e7a24381ebd8b8a10e6be79c33e54'); }
+            });
+            products += '<td>' + xhr.objects[i].qty_ordered + '</td></tr>';
+          });
+          products += '</tbody></table>';
+          $('#order-info').html(products);
+        },
+        error: function(xhr, status){ alert(status + ' ' + xhr.responseText); },
+        beforeSend: function(xhr){ xhr.setRequestHeader('Authorization', 'ApiKey vince:51ec739d7e8e7a24381ebd8b8a10e6be79c33e54'); }
       });
 
   });
