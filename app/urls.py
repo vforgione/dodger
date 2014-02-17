049c17cc3cd41bcc7f59cbdbb016cad1fd340042
vforgione <v.forgione@gmail.com>
2014-02-17 10:59:36 -0600

    finished views/templates refactor

--- app/urls.py
+++ app/urls.py
@@ -5,17 +5,17 @@ urlpatterns = patterns(
     'app.views',
 
     # sku quantity change trace
-    url(r'^sku-quantity-adjustment(?:/(?P<pk>\d+))?/$', 'sku_qty_adj__view', name='sku_qty_adj__view'),
-    url(r'^sku-quantity-adjustment/create/$', 'sku_qty_adj__create', name='sku_qty_adj__create'),
+    url(r'^sku-quantity-adjustment(?:/(?P<pk>\d+))?/$', 'warehouse.sku_qty_adj__view', name='sku_qty_adj__view'),
+    url(r'^sku-quantity-adjustment/create/$', 'warehouse.sku_qty_adj__create', name='sku_qty_adj__create'),
 
     # purchase orders
-    url(r'^purchase-order(?:/(?P<pk>\d+))?/$', 'po__view', name='po__view'),
-    url(r'^purchase-order/create/$', 'po__create', name='po__create'),
+    url(r'^purchase-order(?:/(?P<pk>\d+))?/$', 'dat.po__view', name='po__view'),
+    url(r'^purchase-order/create/$', 'dat.po__create', name='po__create'),
 
     # shipments
-    url(r'^shipment(?:/(?P<pk>\d+))?/$', 'shipment__view', name='shipment__view'),
-    url(r'^shipment/create/$', 'shipment__create', name='shipment__create'),
+    url(r'^shipment(?:/(?P<pk>\d+))?/$', 'warehouse.shipment__view', name='shipment__view'),
+    url(r'^shipment/create/$', 'warehouse.shipment__create', name='shipment__create'),
 
     # skus
-    url(r'^sku(?:/(?P<pk>\d+))?/$', 'sku__view', name='sku__view'),
+    url(r'^sku(?:/(?P<pk>\d+))?/$', 'skus.sku__view', name='sku__view'),
 )
