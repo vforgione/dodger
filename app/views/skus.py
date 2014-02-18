c1abda41a2b21b6368d33a633e40e6ae2d46139c
vforgione <v.forgione@gmail.com>
2014-02-18 15:25:27 -0600

    paring donw unnecessary stuff

--- app/views/skus.py
+++ app/views/skus.py
@@ -4,8 +4,8 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 
-from app.forms import SkuForm, SkuAttributeFormset, SupplierForm
-from app.models import Sku, Supplier
+from app.forms import SkuForm, SkuAttributeFormset
+from app.models import Sku
 
 
 PAGE_SIZE = 20
@@ -95,7 +95,6 @@ def sku__update(request, pk=None):
     if request.method == 'GET':
         form = SkuForm(instance=sku)
         formset = SkuAttributeFormset(instance=sku)
-        # TODO: optimize brands based on supplier
 
     else:
         form = SkuForm(request.POST, instance=sku)
@@ -114,87 +113,4 @@ def sku__update(request, pk=None):
             'form': form,
             'formset': formset,
         }
-    )
-
-
-# suppliers
-@login_required
-def supplier__view(request, pk=None):
-    supplier, suppliers = None, None
-
-    if pk:
-        supplier = get_object_or_404(Supplier, pk=pk)
-
-    else:
-        suppliers = Supplier.objects.order_by('name')
-        paginator = Paginator(suppliers, PAGE_SIZE)
-        page = request.GET.get('page', 1)
-        try:
-            suppliers = paginator.page(page)
-        except PageNotAnInteger:
-            suppliers = paginator.page(1)
-        except EmptyPage:
-            suppliers = paginator.page(paginator.num_pages)
-
-    return render_to_response(
-        'app/skus/supplier__view.html',
-        {
-            'supplier': supplier,
-            'suppliers': suppliers,
-        },
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required
-def supplier__create(request):
-    form = SupplierForm()
-
-    if request.method == 'POST':
-        form = SupplierForm(request.POST)
-        if form.is_valid():
-            return redirect(reverse('app:supplier__view'))
-
-    return render_to_response(
-        'app/skus/supplier__create.html',
-        {
-            'form': form,
-        },
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required
-def supplier__update(request, pk=None):
-    suppliers, form = None, None
-
-    if pk is None:
-        suppliers = Supplier.objects.order_by('-id')
-        paginator = Paginator(suppliers, PAGE_SIZE)
-        page = request.GET.get('page', 1)
-        try:
-            suppliers = paginator.page(page)
-        except PageNotAnInteger:
-            suppliers = paginator.page(1)
-        except EmptyPage:
-            suppliers = paginator.page(paginator.num_pages)
-
-    if request.method == 'GET':
-        if pk:
-            form = SupplierForm(instance=get_object_or_404(Supplier, pk=pk))
-        else:
-            form = SupplierForm()
-
-    else:
-        form = SupplierForm(request.POST)
-        if form.is_valid():
-            return redirect(reverse('app:supplier__view'))
-
-    return render_to_response(
-        'app/skus/supplier__update.html',
-        {
-            'form': form,
-            'suppliers': suppliers,
-        },
-        context_instance=RequestContext(request)
     )
