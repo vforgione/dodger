dacb651a7f5b87dbdec3062ea04f685c376d8afc
vforgione <v.forgione@gmail.com>
2014-02-18 15:43:54 -0600

    cleaned up urls

--- app/urls.py
+++ app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     # purchase orders
     url(r'^purchase-order(?:/(?P<pk>\d+))?/$', 'dat.po__view', name='po__view'),
     url(r'^purchase-order/create/$', 'dat.po__create', name='po__create'),
+
     # reporting
     url(r'purchase-order/export/$', 'reporting.po__export', name='po__export'),
     url(r'purchase-order/csv/(?P<start>\d{4}\-\d{2}\-\d{2})/(?P<end>\d{4}\-\d{2}\-\d{2})/$',
