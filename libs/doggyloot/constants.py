e00bfe61522ce1b09de3751ee329cf1b8569aa68
vforgione <v.forgione@gmail.com>
2014-03-07 08:47:46 -0600

    updated sd api endpoint -- fixes #68

--- libs/doggyloot/constants.py
+++ libs/doggyloot/constants.py
@@ -1,4 +1,4 @@
-URI_BASE = 'http://localhost:3000/api/v1'
+URI_BASE = 'https://doggyloot.com/api/v1'
 
 URI_LIVE_SKUS = URI_BASE + '/live-skus'
 URI_LIVE_DEALS_WITH_SKUS = URI_BASE + '/live-deals-with-skus'
@@ -9,8 +9,4 @@ URI_DONE_DEALS_WITH_SKUS = URI_BASE + '/done-deals-with-skus'
 
 NOTIFICATION_RECIPIENTS = [
     'vince@doggyloot.com',
-    # 'zack@doggyloot.com',
-    # 'pj@doggyloot.com',
-    # 'torin@doggyloot.com',
-    # 'ed@doggyloot.com',
 ]
