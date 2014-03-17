f0e4ed8d2618e39267867a4b58ed58d6b8549d46
vforgione <v.forgione@gmail.com>
2014-03-17 16:50:53 -0500

    moving to live fire update from tracker in master

--- libs/tracker/inventory.py
+++ libs/tracker/inventory.py
@@ -171,10 +171,10 @@ def process_doc(doc):
 
 
 if __name__ == '__main__':
-    import json
-    # from gdocs_config import *
-    # doc = get_doc(USERNAME, PASSWORD, DOC_NAME, SHEET_NAME)
+    # import json
+    from gdocs_config import *
+    doc = get_doc(USERNAME, PASSWORD, DOC_NAME, SHEET_NAME)
     # with open('/tmp/tracker.json', 'w') as fh:
     #     json.dump(doc, fh)
-    doc = json.load(open('/tmp/tracker.json', 'r'))
+    # doc = json.load(open('/tmp/tracker.json', 'r'))
     process_doc(doc)
