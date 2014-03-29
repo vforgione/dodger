b24605833daab18430b8ea4fb7d1122744c1ae5b
vforgione <v.forgione@gmail.com>
2014-03-29 17:39:46 -0500

    renamed and updated uwsgi-run

--- config/uwsgi-run.sh
+++ config/uwsgi-run.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+echo "killing wsgi processes"
+ps -ef | grep wsgi | awk '{print $2}' | xargs kill -9
+
+echo "starting mew wsgi processes"
 NEW_RELIC_CONFIG_FILE=/home/deploy/dodger-env/dodger/config/newrelic.ini newrelic-admin run-program uwsgi --ini /home/deploy/dodger-env/dodger/config/uwsgi.ini --single-interpreter --enable-threads
